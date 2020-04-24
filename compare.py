981cbe1eb1878b33808d255b2dfec472c417ffbe
Jasper van den Bosch <japsai@gmail.com>
2020-04-24 13:47:33 +0200

    figured out lower SF

--- compare.py
+++ compare.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 plt.ion()
 
 
-img = cv2.imread('images/zebras_200.jpg')   ## 3 channels, uint8
+img = cv2.imread('images/dog_200.png')   ## 3 channels, uint8
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) ## 1 channel, uint8
 
 ## show image
@@ -15,13 +15,18 @@ plt.imshow(img, cmap='gray', vmin=0, vmax=255)
 plt.show()
 # %timeit
 
-ksize = (100, 100) # ksize = (5, 5)      ##  Size of the filter returned.
-sigma = 3           ##  Standard deviation of the gaussian envelope.
-theta = 2 * pi/4    ##  Orientation of the normal to the parallel stripes of a Gabor function.
-lambd = 1 * pi/4    ##  Wavelength of the sinusoidal factor.
-gamma = 0.4         ##  Spatial aspect ratio.
+
+"""The key is to adapt the sigma and the kernel size to the lambda. Together they make up the SF
+"""
+
+ksize = (121, 121)    ##  Size of the filter returned.
+sigma = 20           ##  Standard deviation of the gaussian envelope.
+theta = 1 * pi/4    ##  Orientation of the normal to the parallel stripes of a Gabor function.
+lambd = 60 #60 # * pi/4   ##  Wavelength of the sinusoidal factor.
+gamma = 0.5         ##  Spatial aspect ratio. ("ellipsicity") 1 is round, 0 straight line
 psi = 0             ##  Phase offset.
-ktype = cv2.CV_32F  ##  Type of filter coefficients. It can be CV_32F or CV_64F .
+ktype = cv2.CV_32F  ##  Type of filter coefficients. It can be CV_32F or CV_64F .(float32)
+
 
 kernel = cv2.getGaborKernel(ksize, sigma, theta, lambd, gamma, psi, ktype)
 
