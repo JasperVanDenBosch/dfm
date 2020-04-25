447f380597dbc768e507ca6f018169064d15a080
Jasper van den Bosch <japsai@gmail.com>
2020-04-25 19:08:38 +0200

    found solution to mag/phase issue

--- compare.m
+++ compare.m
@@ -1,5 +1,5 @@
 img = imread('images/dog_200.png');
-img = img/255;
+%img = img/255;
 
 sf = 4.5;
 wl = 200/sf;
@@ -8,7 +8,8 @@ wl = 200/sf;
 g = gabor(wl,0);
 
 
-filt_img = imgaborfilt(img, wl, 0);
+[mag, phase] = imgaborfilt(img, wl, 0);
+filt_img = mag .* sin(phase)
 
 figure()
 subplot(1,3,1)
@@ -17,3 +18,6 @@ subplot(1,3,2)
 imagesc(filt_img)
 subplot(1,3,3)
 imagesc(img)
+
+
+% plot the kernel size chose by lambda
--- compare.py
+++ compare.py
@@ -3,6 +3,13 @@
 very useful: http://matlabserver.cs.rug.nl/edgedetectionweb/web/edgedetection_params.html
 
 scale of kernel values wrt image also important
+
+todo:
+- restrict theta to 180 degrees
+
+TO READ: https://stackoverflow.com/q/61317974/708221
+
+
 """
 
 import cv2
@@ -15,7 +22,7 @@ import itertools
 
 plotdir = 'plots'
 img = cv2.imread('images/dog_200.png')   ## 3 channels, uint8
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)/255 ## 1 channel, uint8
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)/255 ## 1 channel
 
 n_sfs = 10
 n_oris = 8
@@ -34,16 +41,17 @@ for f, (theta, sf) in enumerate(features):
     theta = theta           ##  Orientation of the normal to the parallel stripes of a Gabor function.
     lambd = size/sf         ##  Wavelength of the sinusoidal factor.
     sigma = 0.56*lambd  # 0.56   ##  Standard deviation of the gaussian envelope. 0.56*lambd
-    kside = 1 + (15 * int(ceil(lambd))) ##  Size of the filter returned. should be a fn of sigma & lambda
+    kside = 1 + (16 * int(ceil(lambd))) ##  Size of the filter returned. should be a fn of sigma & lambda
     gamma = 0.5             ##  Spatial aspect ratio. ("ellipsicity") 1 is round, 0 straight line
     psi = 0                 ##  Phase offset.
     ktype = cv2.CV_32F      ##  Type of filter coefficients. It can be CV_32F or CV_64F .(float32)
 
     kernel = cv2.getGaborKernel((kside, kside), sigma, theta, lambd, gamma, psi, ktype)
-
+    kernel /= numpy.sqrt((kernel * kernel).sum())
 
     ## filter
     filt_img = cv2.filter2D(img, -1, kernel)
+    #filt_img = scipy.signal.fftconvolve(img, kernel, mode='same')
     # raise ValueError
 
     ## display filtered img
@@ -52,13 +60,13 @@ for f, (theta, sf) in enumerate(features):
     kernel_imsize = numpy.zeros([size, size])
     offset = abs(int((size-(kside-1))/2)) ## this only works if size is even
     if kside < size:
-        kernel_imsize[offset-2:-offset+1, offset-2:-offset+1] = 1
+        kernel_imsize[offset-2:-offset+1, offset-2:-offset+1] = kernel.max()
         kernel_imsize[offset-1:-offset, offset-1:-offset] = kernel
     else:
         kernel_imsize = kernel[offset:-offset, offset:-offset]
-    axes[0].imshow(kernel_imsize, cmap='gray')
-    axes[1].imshow(filt_img, cmap='gray', vmin=0, vmax=filt_img.max()) # , vmin=0, vmax=255
-    axes[2].imshow(img, cmap='gray', vmin=0, vmax=255)
+    axes[0].imshow(kernel_imsize, cmap='gray', vmin=0)
+    axes[1].imshow(filt_img, cmap='gray', vmin=0, vmax=1) # , vmin=0, vmax=255
+    axes[2].imshow(img*255, cmap='gray', vmin=0, vmax=255)
     fig.suptitle(f'{theta_degrees}°  {sf:.1f} cycles')
     plt.savefig(join(plotdir, f'{f+1}.png'))
     plt.close(fig)
